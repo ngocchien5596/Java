9cf2dfc420ec097c57d64307790a272364a8b9a6
ngocchien5596 <buingocchien5596@gmail.com>
2023-04-07 17:59:34 +0700

    option browser

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -3,6 +3,18 @@ import pytest
 
 
 @pytest.fixture()
-def setup():
-    driver = webdriver.Chrome()
+def setup(browser):
+    if browser == 'chrome':
+        driver = webdriver.Chrome()
+        print("\nLaunching Chorme browser")
+    # elif browser == 'firefox':
+    #     driver = webdriver.FireFox()
     return driver
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request):
+    return request.config.getoption("--browser")
+
